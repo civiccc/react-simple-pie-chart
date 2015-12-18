39c366b8b411eb7816de39a0543abf335451f1b7
Joe Lencioni <joe.lencioni@gmail.com>
2015-12-18 11:32:57 -0800

    Extract renderPaths() function to top level
    Now that this function is pure, we can move it out of the class
    definition. This might give us a small performance win, and it will make
    it easier to convert the class to a pure functional component when we
    get around to it.

--- src/pie_chart.jsx
+++ src/pie_chart.jsx
@@ -7,70 +7,69 @@ const center = size / 2;
 const radius = center - 1; // padding to prevent clipping
 
 /**
+ * @param {Object[]} slices
+ * @return {Object[]}
+ */
+function renderPaths(slices) {
+  const total = slices.reduce((totalValue, { value }) => totalValue + value, 0);
+
+  let radSegment = 0;
+  let lastX = radius;
+  let lastY = 0;
+
+  return slices.map(({ color, value }, index) => {
+    // Should we just draw a circle?
+    if (value === total) {
+      return (
+        <circle
+          r={radius}
+          cx={center}
+          cy={center}
+          fill={color}
+          key={index}
+        />
+      );
+    }
+
+    if (value === 0) {
+      return;
+    }
+
+    const valuePercentage = value / total;
+
+    // Should the arc go the long way round?
+    const longArc = (valuePercentage <= 0.5) ? 0 : 1;
+
+    radSegment += valuePercentage * radCircumference;
+    const nextX = Math.cos(radSegment) * radius;
+    const nextY = Math.sin(radSegment) * radius;
+
+    // d is a string that describes the path of the slice.
+    // The weirdly placed minus signs [eg, (-(lastY))] are due to the fact
+    // that our calculations are for a graph with positive Y values going up,
+    // but on the screen positive Y values go down.
+    const d = [
+      `M ${center},${center}`,
+      `l ${lastX},${-lastY}`,
+      `a${radius},${radius}`,
+      '0',
+      `${longArc},0`,
+      `${nextX - lastX},${-(nextY - lastY)}`,
+      'z',
+    ].join(' ');
+
+    lastX = nextX;
+    lastY = nextY;
+
+    return <path d={d} fill={color} key={index} />;
+  });
+}
+
+/**
  * Generates an SVG pie chart.
  * @see {http://wiki.scribus.net/canvas/Making_a_Pie_Chart}
  */
 export default class PieChart extends React.Component {
-  /**
-   * @param {Object[]} slices
-   * @return {Object[]}
-   */
-  _renderPaths(slices) {
-    const total = slices.reduce(
-      (totalValue, { value }) => totalValue + value, 0);
-
-    let radSegment = 0;
-    let lastX = radius;
-    let lastY = 0;
-
-    return slices.map(({ color, value }, index) => {
-      // Should we just draw a circle?
-      if (value === total) {
-        return (
-          <circle
-            r={radius}
-            cx={center}
-            cy={center}
-            fill={color}
-            key={index}
-          />
-        );
-      }
-
-      if (value === 0) {
-        return;
-      }
-
-      const valuePercentage = value / total;
-
-      // Should the arc go the long way round?
-      const longArc = (valuePercentage <= 0.5) ? 0 : 1;
-
-      radSegment += valuePercentage * radCircumference;
-      const nextX = Math.cos(radSegment) * radius;
-      const nextY = Math.sin(radSegment) * radius;
-
-      // d is a string that describes the path of the slice.
-      // The weirdly placed minus signs [eg, (-(lastY))] are due to the fact
-      // that our calculations are for a graph with positive Y values going up,
-      // but on the screen positive Y values go down.
-      const d = [
-        `M ${center},${center}`,
-        `l ${lastX},${-lastY}`,
-        `a${radius},${radius}`,
-        '0',
-        `${longArc},0`,
-        `${nextX - lastX},${-(nextY - lastY)}`,
-        'z',
-      ].join(' ');
-
-      lastX = nextX;
-      lastY = nextY;
-
-      return <path d={d} fill={color} key={index} />;
-    });
-  }
-
   /**
    * @return {Object}
    */
@@ -78,7 +77,7 @@ export default class PieChart extends React.Component {
     return (
       <svg viewBox={`0 0 ${size} ${size}`}>
         <g transform={`rotate(-90 ${center} ${center})`}>
-          {this._renderPaths(this.props.slices)}
+          {renderPaths(this.props.slices)}
         </g>
       </svg>
     );
